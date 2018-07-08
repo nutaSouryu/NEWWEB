7e3896471e9e1fca0e79adda4feb39b792026383
nutaSouryu <souryu.nuta@gmail.com>
2018-07-08 17:09:34 +0900

    テストプログラム

---
+++ jp.co.gaiarai/src/jp/co/gaiarai/obj/Matango.java
@@ -0,0 +1,10 @@
+package jp.co.gaiarai.obj;
+
+public class Matango {
+	public int hp;
+	public final int level = 10;
+	public char suffix;
+	public void run() {
+		System.out.println("お化けキノコ"+this.suffix+"は逃げ出した！");
+	}
+}
---
+++ jp.co.gaiarai/src/jp/co/gaiarai/obj/Sword.java
@@ -0,0 +1,6 @@
+package jp.co.gaiarai.obj;
+
+public class Sword {
+	public String name;
+	public int damage;
+}
